5be7703397bb5b50449351e35f420a9e2a7e4816
ry <rnakada90@gmail.com>
2019-01-15 20:10:14 -0500

    updatedHTML

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-
-$(".sendButton").on("click", function(event){
-
-event.preventDefault();
-
-var movieTitle = $(".movie-input").val();
-var queryURL = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=95f43ed4"
-// var queryURL = "https://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=95f43ed4";
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-
-    console.log(response);
-
-    $(".movieText").text(JSON.stringify(response));
-
-    console.log(response.Ratings);
-    console.log(response.Ratings[1]);
-
-});
-
-});
-
-// 95f43ed4
-// http://www.omdbapi.com/?apikey=[yourkey]&
-
-// http://www.omdbapi.com/?t=taken
-=======
 var header = $("#header");
 var submit = $("#submit");
 var UserName = $("#UserName");
@@ -36,7 +6,7 @@ var mainContent = $("#mainContent");
 var categories = $("#categories");
 var name;
 
-submit.on('click', function(){
+submit.on('click', function () {
     var newCat = $("<div>");
     newCat.attr('class', 'NewButton');
     name = UserName.val().trim();
@@ -46,5 +16,25 @@ submit.on('click', function(){
     categories.append(newCat);
 });
 
+$("#submit").on("click", function (event) {
 
->>>>>>> 441dca96a98fd96d35d45e73cc1155c85f41d5a0
+    event.preventDefault();
+
+    var movieTitle = $("").val();
+    var queryURL = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=95f43ed4"
+    // var queryURL = "https://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=95f43ed4";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        console.log(response);
+
+        $(".movieText").text(JSON.stringify(response));
+
+        console.log(response.Ratings);
+        console.log(response.Ratings[1]);
+
+    });
+
+});
